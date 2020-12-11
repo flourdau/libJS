cff5c870ee7d809cc48476a8403703ac64ad1aa2
Florian LOURDAUX <floodeb@live.nl>
2020-12-11 17:36:16 +0100

    Amélio Shell

--- myShell.js
+++ myShell.js
@@ -1,54 +1,92 @@
-// TPGGLE: 
-const buttonClose = document.querySelector("#myShell>div>div:first-child>ul:last-child>li:last-child");
-const buttonMax = document.querySelector("#myShell>div>div:first-child>ul:last-child>li:nth-child(2)");
-const buttonMin = document.querySelector("#myShell>div>div:first-child>ul:last-child>li:first-child");
-const buttonBar = document.querySelector("#myShell>div>div:first-child>ul:first-child>li:last-child");
-const buttonOpacity = document.querySelector("#myShell>div>div:first-child>ul:first-child>li:first-child");
+// TOGGLE:
+const miniBarMenu = document.querySelector("main>div>section:last-child");
+const buttonClose = document.getElementsByClassName("btnClose");
+const buttonMax = document.getElementsByClassName("btnMaximize");
+const buttonMin = document.getElementsByClassName("btnMinimize");
+const buttonBar = document.getElementsByClassName("#myShell>div>div:first-child>ul:first-child>li:last-child");
+const buttonOpacity = document.getElementsByClassName("btnCode");
+const miniButtonMax = document.getElementsByClassName("miniBtnMaximize");
 
-function myMin() {
-    var barMenu = document.querySelector("#myShell>div>div:first-child");
-    var element = document.querySelector("#myShell>div>div:last-child");
 
-    barMenu.classList.toggle("myBar");
-    element.classList.toggle("myDisplayNone");
+function myClose() {
+    var myInfoClient = document.querySelector("#myInfoClient");
+
+    myInfoClient.setAttribute('class', 'myDisplayNone');
+    myReadMe.setAttribute('class', 'myDisplayNone');
+    myShell.setAttribute('class', 'myDisplayNone'); 
+    myContentClock.style.display = "flex";
+    miniBarMenu.style.display = "none";
 }
 
-buttonMin.addEventListener("click", () => { myMin(); }, false);
+function myMin() {
+    var myInfoClient = document.querySelector("#myInfoClient");
+    var myMiniBar = document.querySelector("main>div>section:last-child");
 
-buttonBar.addEventListener("click", () => { myMin(); }, false);
+    myMiniBar.style.display = "flex";
+    myInfoClient.setAttribute('class', 'myDisplayNone');
+    myReadMe.setAttribute('class', 'myDisplayNone');
+    myShell.setAttribute('class', 'myDisplayNone');
+}
 
-buttonMax.addEventListener('click', {
-    handleEvent: function (event) {
-        var barMenu = document.querySelector("#myShell>div>div:first-child");
-        var element = document.querySelector("#myShell>div>div:last-child");
+function myMax() {
+    var myInfoClient = document.querySelector("#myInfoClient");
+    var myMiniBar = document.querySelector("main>div>section:last-child");
+    var myShell = document.querySelector("#myShell");
 
-        barMenu.classList.toggle("myMax");
-        element.classList.toggle("myMax");
-    }
-});
+    myMiniBar.style.display = "none";
+    myInfoClient.setAttribute('class', 'myDisplay');
+    myReadMe.setAttribute('class', 'myDisplay');
+    myShell.setAttribute('class', 'myDisplay');
+    myAncre();
+}
 
-buttonOpacity.addEventListener('click', {
-    handleEvent: function (event) {
-        var barMenu = document.querySelector("#myShell>div>div:first-child");
-        var element = document.querySelector("#myShell>div>div:last-child");
 
-        if (barMenu.getAttribute('class') === 'myBgTransparent') {
-            barMenu.setAttribute('class', 'myBarBgColor');
-            element.setAttribute('class', 'myShellBgColor'); 
+for (i = 0; i < buttonClose.length; i++) {
+    buttonClose[i].addEventListener("click", function() {myClose();});
+}
+
+for (i = 0; i < buttonOpacity.length; i++) {
+    buttonOpacity[i].addEventListener('click', {
+        handleEvent: function (event) {
+            var barMenu = document.querySelector("#myShell>div>div:first-child");
+            var myShell = document.querySelector("#myShell>div>div:last-child");
+
+            if (barMenu.getAttribute('class') === 'myBgTransparent') {
+                barMenu.setAttribute('class', 'myBarBgColor');
+                myShell.setAttribute('class', 'myShellBgColor');
+            }
+            else {
+                barMenu.setAttribute('class', 'myBgTransparent');
+                myShell.setAttribute('class', 'myBgTransparent'); 
+            }}
+    });
+}
+
+for (i = 0; i < buttonMin.length; i++) {
+    buttonMin[i].addEventListener("click", () => { myMin(); }, false);
+}
+
+for (i = 0; i < buttonBar.length; i++) {
+    buttonBar[i].addEventListener("click", () => { myMin(); }, false);
+}
+
+for (i = 0; i < buttonMax.length; i++) {
+    buttonMax[i].addEventListener('click', {
+        handleEvent: function (event) {
+
+            if (myContentClock.getAttribute('style') === 'display: none;') {
+                myContentClock.style.display = "flex";
+            }
+            else {
+                myContentClock.style.display = "none";
+            }
+
         }
-        else {
-            barMenu.setAttribute('class', 'myBgTransparent');
-            element.setAttribute('class', 'myBgTransparent'); 
-        }}
-});
+    });
+}
 
-buttonClose.addEventListener('click', {
-    handleEvent: function (event) {
-        var element = document.querySelector("#myShell");
-        var myClt = document.querySelector("#myClt");
-
-        myClt.setAttribute('class', 'myDisplayNone');
-        myReadMe.setAttribute('class', 'myDisplayNone');
-        myShell.setAttribute('class', 'myDisplayNone'); 
-    }
-});
+for (i = 0; i < miniButtonMax.length; i++) {
+    miniButtonMax[i].addEventListener('click', {
+        handleEvent: function (event) {myMax();}
+    });
+}
